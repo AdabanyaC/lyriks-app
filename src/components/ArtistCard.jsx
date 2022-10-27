3935ba5656c4af62edad2592d2ddcc292d8ebb77
Clinton Adabanya <clintonadabanya@gmail.com>
2022-10-27 14:46:59 +0100

    Discover component, Top Artist Component and Searchbar component

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,5 +1,23 @@
-const ArtistCard = () => {
-  <div>ArtistCard</div>;
+import { useNavigate } from "react-router-dom";
+
+const ArtistCard = ({ track }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className="flex flex-col p-4 bg-opacity-80 w-[250px] bg-white/5 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer"
+      onClick={() => navigate(`artists/${track?.artists[0].adamid}`)}
+    >
+      <img
+        src={track?.images?.coverart}
+        alt="artist"
+        className="w-full h-56 rounedd-lg"
+      />
+      <p className="mt-4 font-semibold text-lg text-white truncate">
+        {track?.subtitle}
+      </p>
+    </div>
+  );
 };
 
 export default ArtistCard;
